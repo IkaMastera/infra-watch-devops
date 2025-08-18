db29e12d8bc5f0874a831b42e243a7561ce7c8ac
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-08-18 14:47:27 +0400

    add: ping.py minimal logic

--- utils/ping.py
+++ utils/ping.py
@@ -1,3 +1,14 @@
 import platform
 import subprocess
-from typing import Tuple
+
+def ping_host(host: str) -> bool:
+    flag = "-n" if platform.system().lower().startswith("win") else "-c"
+    cmd = ["ping", flag, "1", host]
+
+    result = subprocess.run(
+        cmd,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+    return result.returncode == 0
